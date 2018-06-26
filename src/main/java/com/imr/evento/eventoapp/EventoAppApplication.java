311e7f643ef7b29aa6903e4838ed496f20df33e1
imranjdev <imransoudagar.jdev@gmail.com>
2018-06-26 11:30:50 +0530

    Initial commit

---
+++ src/main/java/com/imr/evento/eventoapp/EventoAppApplication.java
@@ -0,0 +1,12 @@
+package com.imr.evento.eventoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoAppApplication.class, args);
+	}
+}
